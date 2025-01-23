6cfbb5af670cf413c51a420df34b9dd144d26c7e
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2025-01-23 16:51:15 +0000

    gallery test

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,22 +1,32 @@
-<script lang='ts'>
+<script lang="ts">
 	import { goto } from '$app/navigation';
 	import P5wrapper from '$lib/P5wrapper.svelte';
 	import { width, height } from '$lib/store';
+	import input from '$lib/logos/input.webp';
+	import chat from '$lib/images/chat.webp';
+	import image from '$lib/images/image.webp';
+	import image2 from '$lib/images/image2.webp';
+	import model from '$lib/images/model.webp';
+	import video from '$lib/images/video.webp';
+	import vector from '$lib/images/vector.webp';
+	import web from '$lib/images/web.webp';
+	import { slide, fade } from 'svelte/transition';
 
-	let hueRotation = $state(0)
+	let textarea: any = $state();
+	let hueRotation = $state(0);
 
 	// Define your sketch function
 	function sketch(p: any, appCanvas: any) {
 		let grid: any;
 		let cellSize = 20;
 		let gridSizeX = Math.round($width / cellSize);
-		let gridSizeY = Math.round($height / cellSize);
+		let gridSizeY = Math.round(($height * 0.8) / cellSize);
 		let iteration = 0; // Counter variable
 		let maxIterations = 10; // Maximum number of iterations
 		p.setup = function () {
 			cellSize = 20;
-		 	gridSizeX = Math.round($width / cellSize);
-		 	gridSizeY = Math.round($height / cellSize);
+			gridSizeX = Math.round($width / cellSize);
+			gridSizeY = Math.round($height * 0.8 / cellSize);
 			iteration = 0; // Counter variable
 			maxIterations = 20; // Maximum number of iterations
 			p.createCanvas(gridSizeX * cellSize, gridSizeY * cellSize);
@@ -29,12 +39,12 @@
 				}
 			}
 			// Randomly select 10 cells to have a value of 1
-			// for (let i = 0; i < 50; i++) {
-			// 	const x = p.floor(p.random(gridSizeX));
-			// 	const y = p.floor(p.random(-5, 5) + gridSizeY / 2);
-			// 	// const y = gridSizeY / 2
-			// 	grid[x][y] = 0;
-			// }
+			for (let i = 0; i < 10; i++) {
+				const x = p.floor(p.random(-5, 5) + gridSizeX / 2);
+				const y = p.floor(p.random(-5, 5) + gridSizeY / 2);
+				// const y = gridSizeY / 2
+				grid[x][y] = 20;
+			}
 			// grid[gridSizeX/2][gridSizeY/2] = 1;
 		};
 		p.draw = function () {
@@ -56,7 +66,7 @@
 			for (let i = 0; i < gridSizeX; i++) {
 				for (let j = 0; j < gridSizeY; j++) {
 					grid[i][j] -= 0.005;
-					if(grid[i][j] < 0){
+					if (grid[i][j] < 0) {
 						grid[i][j] = 0;
 					}
 				}
@@ -64,9 +74,9 @@
 			for (let i = 0; i < gridSizeX; i++) {
 				for (let j = 0; j < gridSizeY; j++) {
 					// const value = p.map(grid[i][j], minVal, maxVal, 0.1, 1);
-					const value = grid[i][j]
-					let saturation = value > 0.1 ? 90 : 0
-					let lightness = value > 0.1 ? 75 : 98
+					const value = grid[i][j];
+					let saturation = value > 0.1 ? 90 : 0;
+					let lightness = value > 0.1 ? 75 : 98;
 					p.fill(`hsl(${Math.round(value * 180 + 150)}, ${saturation}%, ${lightness}%)`);
 					p.noStroke();
 					p.rect(i * cellSize, j * cellSize, cellSize, cellSize);
@@ -120,78 +130,166 @@
 			p.loop();
 		};
 	}
+
+	let galleryItem = $state(0);
+	const gallery = [
+		{
+			image: chat,
+			text: `Do you know a design technique when a building design is inspired by a crumpled piece of paper?`
+		},
+		{
+			image: image,
+			text: `Create an image of a crumpled piece of paper`
+		},
+		{
+			image: image2,
+			text: `Create an image of a modern opera building based on the composition reference image.`
+		},
+		{ image: model, text: `Turn this image in a 3D model.` },
+		{ image: video, text: `Turn this image in a video.` },
+		{
+			image: vector,
+			text: `Create a fun vector illustration of a modern opera building inspired by Frank Gehry style.`
+		},
+		{
+			image: web,
+			text: `I want to create a game where a player has to run a modern opera building. Can you help me with this?`
+		}
+	];
+	const totalImages = gallery.length;
+
+	function scrollToImage(index: number) {
+		galleryItem = index;
+	}
+
+	// Automatically change images every 3 seconds
+	setInterval(() => {
+		scrollToImage((galleryItem + 1) % totalImages);
+	}, 5000);
 </script>
 
 <div class="start-page-container">
-	<div class="hero">
-		<h1 class='primaryHeading'>Your creativity supercharged with AI.</h1>
-		<p>
-			Brainstorm, create visuals, generate web apps, and more — all with just text. No complex
-			setups and expensive hardware requirements.
-		</p>
-		<button class="primaryButton">Get started - it's free</button>
-
-		<div class="sketchWrapper" style='filter: blur(20px) hue-rotate({hueRotation}deg);'>
-			<!-- Use the P5wrapper component and pass the sketch function -->
-			<P5wrapper {sketch} />
+	<div class="start-page-wrapper">
+		<div class="hero">
+			<div class="hero-wrapper">
+				<h1 class="primaryHeading">Your creativity supercharged with AI.</h1>
+				<p>
+					Brainstorm, create visuals, generate web apps, and more — all with just text. No complex
+					setups and expensive hardware requirements.
+				</p>
+				<button class="primaryButton">Get started - it's free</button>
+			</div>
+			<div class="sketchWrapper" style="filter: blur(20px) hue-rotate({hueRotation}deg);">
+				<!-- Use the P5wrapper component and pass the sketch function -->
+				<P5wrapper {sketch} />
+			</div>
 		</div>
-	</div>
 
-	<div class="section">
-		<h3 class='secondaryHeading'>One interface, every model you need.</h3>
-		<p>Generate text, code, images, videos, and 3D assets — all from one platform.</p>
-	</div>
-	<div class="section">
-		<h3 class='secondaryHeading'>The fast and easy way to create with AI.</h3>
-		<p>
-			No complex workflows or local hardware limits. Everything works in the cloud with the fastest
-			GPUs.
-		</p>
-	</div>
-	<div class="section">
-		<h3 class='secondaryHeading'>Make it a web app.</h3>
-		<p>Generate content and turn it into web apps. Host with us and share with everyone.</p>
-	</div>
+		<div class="section" style='margin-top: 20px;'>
+			<h3 class="secondaryHeading">One interface, every model you need.</h3>
+			<p>Generate text, code, images, videos, and 3D assets — all from one platform.</p>
 
-	<div class="section">
-		<h3 class='secondaryHeading'>AI Agents</h3>
-		<p>
-			Specifically designed to make your creative search and iterations easier and faster. Coming
-			soon.
-		</p>
-	</div>
+			<div class="gallery">
+				<img src={gallery[galleryItem].image} alt="input" style="width: 50%;" transition:slide />
+			</div>
 
-	<div class="section">
-		<h3 class='secondaryHeading'>Tutorials</h3>
-		<p>
-			Learning materials from industry experts. From building your first game to training your first
-			neural network — we’ve got you covered.
-		</p>
-	</div>
+			<div class="chat-panel-container">
+				<textarea
+					bind:this={textarea}
+					placeholder={`Type "?" for manual. Type questions or prompts for images, SVGs, videos, and 3d models.`}
+				>{gallery[galleryItem].text}</textarea>
+				<button
+					class="tertiaryButton"
+					style="width: 40px; height: 40px; disply: flex; justify-content: center; align-items: center;"
+					aria-label="Upload File"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="1"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path
+							d="M21.44 11.05l-9.19 9.19a5.5 5.5 0 0 1-7.78-7.78l9.19-9.19a4 4 0 0 1 5.66 5.66l-8.48 8.48a2 2 0 0 1-2.83-2.83l7.78-7.78"
+						/>
+					</svg>
+				</button>
+				<button
+					id="magicButton"
+					class="primaryButton"
+					onclick={() => {
+						galleryItem++;
+						if(galleryItem >= totalImages){
+							galleryItem = 0;
+						}
+					}}
+				>
+					Go
+				</button>
+			</div>
 
-	<div class="section">
-		<h3 class='secondaryHeading'>Describe Your Task, We’ll Handle the Rest.</h3>
-		<p>
-			Create anything: graphic design, architecture,
-			games, and more.
-		</p>
-	</div>
+			<div class="gallery-buttons">
+				{#each gallery as _, index}
+					<button
+						class:active={index === galleryItem}
+						onclick={() => scrollToImage(index)}
+						aria-label="Next Image"
+					></button>
+				{/each}
+			</div>
+		</div>
 
-	<div class="section">
-		<h3 class='secondaryHeading'>The perfect UI to create with AI.</h3>
-		<p>
-			Stay in your creative flow with no complex setups or
-			controls.
-		</p>
-		<button class="primaryButton">Get started - it's free</button>
-	</div>
+		<div class="section">
+			<h3 class="secondaryHeading">The fast and easy way to create with AI.</h3>
+			<p>
+				No complex workflows or local hardware limits. Everything works in the cloud with the
+				fastest GPUs.
+			</p>
+		</div>
+		<div class="section">
+			<h3 class="secondaryHeading">Make it a web app.</h3>
+			<p>Generate content and turn it into web apps. Host with us and share with everyone.</p>
+		</div>
 
-	<!-- <div class="section">
+		<div class="section">
+			<h3 class="secondaryHeading">AI Agents</h3>
+			<p>
+				Specifically designed to make your creative search and iterations easier and faster. Coming
+				soon.
+			</p>
+		</div>
+
+		<div class="section">
+			<h3 class="secondaryHeading">Tutorials</h3>
+			<p>
+				Learning materials from industry experts. From building your first game to training your
+				first neural network — we’ve got you covered.
+			</p>
+		</div>
+
+		<div class="section">
+			<h3 class="secondaryHeading">Describe Your Task, We’ll Handle the Rest.</h3>
+			<p>Create anything: graphic design, architecture, games, and more.</p>
+		</div>
+
+		<div class="section">
+			<h3 class="secondaryHeading">The perfect UI to create with AI.</h3>
+			<p>Stay in your creative flow with no complex setups or controls.</p>
+			<button class="primaryButton">Get started - it's free</button>
+		</div>
+
+		<!-- <div class="section">
 		<h2>FAQ</h2>
 	</div> -->
 
-	<div class="footer">
-		<p>Contacts | Subscribe | Privacy policy | Terms of service | Social media links</p>
+		<div class="footer">
+			<p>Contacts | Subscribe | Privacy policy | Terms of service | Social media links</p>
+		</div>
 	</div>
 </div>
 
@@ -200,37 +298,112 @@
 		overflow-y: scroll;
 		height: 100vh;
 		height: 100svh;
-		max-width: 1200px;
+		width: 100%;
 		margin: auto;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
-	.hero {
-		text-align: center;
-		padding: 2rem;
-        display: flex;
+	.start-page-wrapper {
+		display: flex;
 		flex-direction: column;
 		align-items: center;
+		max-width: 1200px;
+	}
+	.hero {
+		flex: 0 0 70vh;
+		text-align: center;
+		padding: 2rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		position: relative;
+	}
+	.hero-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.hero-wrapper p {
+		max-width: 600px;
 	}
 	.section {
 		padding: 2rem;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-        text-align: center;
+		text-align: center;
+		position: relative;
 	}
 	.footer {
 		text-align: center;
 		padding: 1rem;
 	}
-	.sketchWrapper{
+	.sketchWrapper {
 		position: absolute;
-		
+
 		filter: blur(10px);
 		-webkit-filter: blur(10px);
 		transition: all 2s;
 		z-index: -1;
+	}
+	.chat-panel-container {
+		width: calc(100% - 20px);
+		max-width: 800px;
+		margin: 20px;
+		padding: 10px;
+		box-sizing: border-box;
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
+		background: linear-gradient(45deg, rgba(255, 255, 255, 0.52), rgba(255, 255, 255, 0.25));
+		backdrop-filter: blur(40px);
+		-webkit-backdrop-filter: blur(25px);
+		border-radius: 10px;
+		box-shadow: 0 0 10px hsl(0, 0%, 70%);
+		/* position: absolute;
+		bottom: 30px; */
+	}
+	textarea {
+		border: none;
+		border-radius: 0;
+		border-bottom: 1px solid hsl(0, 0%, 90%);
+		background: none;
+		background-color: hsla(0, 0%, 0%, 0.05);
+		color: hsl(0, 0%, 10%);
+		font-size: 1rem;
+		font-family: 'Roboto', sans-serif;
+		font-weight: 300;
+		height: 40px;
+		padding: 10px;
+		margin: 0;
+		box-sizing: border-box;
+		width: 100%;
+	}
+	#magicButton {
+		width: 40px;
+		height: 40px;
+	}
+
+	.gallery-buttons {
+		display: flex;
+		justify-content: center;
+		margin-top: 0px;
+	}
+
+	.gallery-buttons button {
+		width: 40px;
+		height: 5px;
+		border-radius: 5px;
+		background-color: hsl(0, 0%, 90%);
+		margin: 0 5px;
+		border: none;
+		cursor: pointer;
+		transition: background-color 0.3s;
+	}
+
+	.gallery-buttons button.active {
+		background-color: hsl(0, 0%, 2%);
 	}
 </style>
