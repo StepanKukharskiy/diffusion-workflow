c107f037e771a8825ba2b31d64487a285d907920
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2025-01-23 18:16:43 +0000

    landing update

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -22,7 +22,7 @@
 		let gridSizeX = Math.round($width / cellSize);
 		let gridSizeY = Math.round(($height * 0.8) / cellSize);
 		let iteration = 0; // Counter variable
-		let maxIterations = 10; // Maximum number of iterations
+		let maxIterations = 20; // Maximum number of iterations
 		p.setup = function () {
 			cellSize = 20;
 			gridSizeX = Math.round($width / cellSize);
@@ -40,9 +40,16 @@
 			}
 			// Randomly select 10 cells to have a value of 1
 			for (let i = 0; i < 10; i++) {
-				const x = p.floor(p.random(-5, 5) + gridSizeX / 2);
+				const x = p.floor(p.random(-10, 10) + gridSizeX / 2);
+
 				const y = p.floor(p.random(-5, 5) + gridSizeY / 2);
-				// const y = gridSizeY / 2
+
+				// let y
+				// if(Math.random() > 0.5){
+				// 	y = 0
+				// } else {
+				// 	y = gridSizeY - 3
+				// }
 				grid[x][y] = 20;
 			}
 			// grid[gridSizeX/2][gridSizeY/2] = 1;
@@ -77,7 +84,7 @@
 					const value = grid[i][j];
 					let saturation = value > 0.1 ? 90 : 0;
 					let lightness = value > 0.1 ? 75 : 98;
-					p.fill(`hsl(${Math.round(value * 180 + 150)}, ${saturation}%, ${lightness}%)`);
+					p.fill(`hsl(${Math.round(value * 100 + 150)}, ${saturation}%, ${lightness}%)`);
 					p.noStroke();
 					p.rect(i * cellSize, j * cellSize, cellSize, cellSize);
 				}
@@ -86,7 +93,7 @@
 			const newGrid = new Array(gridSizeX).fill(0).map(() => new Array(gridSizeY).fill(0));
 			for (let i = 0; i < gridSizeX; i++) {
 				for (let j = 0; j < gridSizeY; j++) {
-					if (grid[i][j] === 10) {
+					if (grid[i][j] === 5) {
 						// If the cell's value is 1, keep it as 1
 						//newGrid[i][j] = 1;
 					} else {
@@ -176,7 +183,7 @@
 	<div class="start-page-wrapper">
 		<div class="hero">
 			<div class="hero-wrapper">
-				<h1 class="primaryHeading">Your creativity supercharged with AI.</h1>
+				<h1 class="primaryHeading">Your creativity supercharged with AI</h1>
 				<p>
 					Brainstorm, create visuals, generate web apps, and more — all with just text. No complex
 					setups and expensive hardware requirements.
@@ -192,7 +199,7 @@
 		<div class="section" style="margin-top: 20px;">
 			<span style="width: 50%; border-top: 1px solid hsl(0, 0%, 90%); margin-bottom: 40px;"></span>
 
-			<h3 class="secondaryHeading">One interface, every task you have.</h3>
+			<h3 class="secondaryHeading">One interface, every task you have</h3>
 			<p>Generate text, code, images, videos, and 3D assets — all from one platform.</p>
 
 			<div class="gallery">
@@ -266,7 +273,7 @@
 				</svg>
 
 				<h3 class="secondaryHeading" style="margin-top: 20px;">
-					The fast and easy way to create with AI.
+					The fast and easy way to create with AI
 				</h3>
 				<p>
 					No complex workflows or local hardware limits. Everything works in the cloud with the
@@ -274,32 +281,49 @@
 				</p>
 			</div>
 			<div class="section">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="30" height="30">
-					<circle cx="32" cy="32" r="30" stroke="black" stroke-width="2" fill="none"/>
-					<path d="M2,32 A30,30 0 0,1 62,32" stroke="black" stroke-width="2" fill="none"/>
-					<path d="M32,2 A30,30 0 0,1 32,62" stroke="black" stroke-width="2" fill="none"/>
-					<path d="M12,12 A30,30 0 0,1 52,52" stroke="black" stroke-width="2" fill="none"/>
-					<path d="M12,52 A30,30 0 0,1 52,12" stroke="black" stroke-width="2" fill="none"/>
-					<line x1="2" y1="32" x2="62" y2="32" stroke="black" stroke-width="2"/>
-					<line x1="32" y1="2" x2="32" y2="62" stroke="black" stroke-width="2"/>
-				  </svg>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="30" height="30"
+					><defs
+						><style>
+							.cls-1 {
+								fill: #201602;
+							}
+						</style></defs
+					><g id="Layer_2" data-name="Layer 2"
+						><g id="layer_1-2" data-name="layer 1"
+							><path
+								class="cls-1"
+								d="M24 48a24 24 0 1 1 24-24 24 24 0 0 1-24 24zm0-46a22 22 0 1 0 22 22A22 22 0 0 0 24 2z"
+							/><path
+								class="cls-1"
+								d="M24 48c-7.85 0-14-10.54-14-24S16.15 0 24 0s14 10.54 14 24-6.15 24-14 24zm0-46c-6.62 0-12 9.87-12 22s5.38 22 12 22 12-9.87 12-22S30.62 2 24 2z"
+							/><path class="cls-1" d="M23 4h2v43h-2z" /><path
+								class="cls-1"
+								d="M1 23h33v2H1zM37 23h10v2H37zM7 12h37v2H7zM4 34h8v2H4zM16 34h28v2H16z"
+							/></g
+						></g
+					></svg
+				>
 
-				<h3 class="secondaryHeading" style="margin-top: 20px;">Make it a web app.</h3>
+				<h3 class="secondaryHeading" style="margin-top: 20px;">Make it a web app</h3>
 				<p>Generate content and turn it into web apps. Host with us and share with everyone.</p>
 			</div>
 
 			<div class="section">
-				<svg
-					class="brain-icon"
-					width="30"
-					height="30"
-					viewBox="0 0 64 64"
-					xmlns="http://www.w3.org/2000/svg"
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="30" height="30"
+					><g data-name="15-idea"
+						><path
+							d="M40 33h-6a1 1 0 0 1-1-1v-3.1a1 1 0 0 0-.6-.9 11 11 0 1 1 9.195 0 1 1 0 0 0-.6.908V32A1 1 0 0 1 40 33zm-5-2h4v-2.1a3.015 3.015 0 0 1 1.762-2.724A9 9 0 0 0 43 11.292a9 9 0 1 0-9.759 14.887A3.014 3.014 0 0 1 35 28.9z"
+						/><path
+							d="M38 39h-2a3 3 0 0 1-3-3v-4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v4a3 3 0 0 1-3 3zm-3-6v3a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-3z"
+						/><path d="M34 34h6v2h-6z" /><path
+							d="M24 48a7 7 0 0 1-6.322-4.008A7.008 7.008 0 0 1 11 37v-.071a7.015 7.015 0 0 1-5.964-6.214 6.994 6.994 0 0 1 0-13.429A7.013 7.013 0 0 1 11 11.071V11a7.01 7.01 0 0 1 6.678-6.993A7.006 7.006 0 0 1 24 0a6.006 6.006 0 0 1 6 6h-2a4 4 0 0 0-4-4 5 5 0 0 0-4.714 3.349.976.976 0 0 1-1 .667l-.1-.007c-.06 0-.12-.009-.182-.009a5.006 5.006 0 0 0-5 5 4.9 4.9 0 0 0 .077.836 1.007 1.007 0 0 1-.227.82 1.028 1.028 0 0 1-.775.348h-.1A5 5 0 0 0 7 18a1.1 1.1 0 0 1-.831 1.076 4.993 4.993 0 0 0 0 9.849 1 1 0 0 1 .835 1v.1A5 5 0 0 0 12 35a1.093 1.093 0 0 1 .81.353 1.024 1.024 0 0 1 .267.81A4.923 4.923 0 0 0 13 37a5.006 5.006 0 0 0 5 5c.062 0 .122-.005.182-.009l.1-.007a1 1 0 0 1 1 .667A5 5 0 0 0 24 46a4 4 0 0 0 4-4 1 1 0 0 1 1-1h6a1 1 0 0 0 1-1v-2h2v2a3 3 0 0 1-3 3h-5.083A6.01 6.01 0 0 1 24 48z"
+						/><path
+							d="M12 25a7.008 7.008 0 0 1-7-7h2a5.006 5.006 0 0 0 5 5zM18 44a7.008 7.008 0 0 1-7-7h2a5 5 0 1 0 5-5v-2a7 7 0 0 1 0 14zM18 18v-2a5 5 0 1 0-5-5h-2a7 7 0 1 1 7 7zM38 32h-2V19a1 1 0 0 1 1-1h3a1 1 0 0 0 1-1v-1h2v1a3 3 0 0 1-3 3h-2z"
+						/><path
+							d="M37 20h-3a3 3 0 0 1-3-3v-1h2v1a1 1 0 0 0 1 1h3zM18 35h2v2h-2zM16 37h2v2h-2zM13 16h2v2h-2zM11 18h2v2h-2zM23 23h2v2h-2zM21 25h2v2h-2z"
+						/></g
+					></svg
 				>
-					<path
-						d="M32 2C20.954 2 12 10.954 12 22c0 4.418 1.79 8.418 4.688 11.312C14.79 36.582 13 40.582 13 45c0 11.046 8.954 20 20 20s20-8.954 20-20c0-4.418-1.79-8.418-4.688-11.312C51.21 30.418 53 26.418 53 22c0-11.046-8.954-20-20-20zm0 2c9.941 0 18 8.059 18 18 0 3.866-1.387 7.418-3.688 10.188C48.613 34.582 50 38.134 50 42c0 9.941-8.059 18-18 18s-18-8.059-18-18c0-3.866 1.387-7.418 3.688-10.188C15.387 29.418 14 25.866 14 22c0-9.941 8.059-18 18-18z"
-					/>
-				</svg>
 				<h3 class="secondaryHeading" style="margin-top: 20px;">AI Agents</h3>
 				<p>
 					Specifically designed to make your creative search and iterations easier and faster.
@@ -309,20 +333,18 @@
 
 			<div class="section">
 				<svg
-					width="30"
-					height="30"
-					viewBox="0 0 64 64"
 					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					viewBox="0 0 128 98"
+					width="50"
+					height="30"
+					style="enable-background:new 0 0 128 128"
+					xml:space="preserve"
+					><path
+						d="M110.6 38.3H106v-3.5c0-1-.8-1.7-1.7-1.8H70.8c-2.7 0-5.2 1.2-6.8 3.3-1.6-2.1-4.1-3.3-6.8-3.3H23.7c-1 0-1.7.8-1.7 1.8v3.5h-4.6c-1 0-1.8.8-1.8 1.8v57c0 1 .8 1.7 1.8 1.8h93.1c1 0 1.7-.8 1.8-1.8V40c0-.9-.8-1.7-1.7-1.7zm-39.8-1.8h31.7v51.9H70.8c-2.9 0-4.6 1.4-5 1.6V41.6c0-.4 0-.7-.1-1.1.5-2.2 2.6-4 5.1-4zm-45.3 0h31.7c2.6 0 4.7 1.9 5 4.1 0 .4-.1.7-.1 1.1V90c-.3-.1-2.1-1.6-5-1.6H25.5V36.5zm-6.3 5.3H22v48.3c0 1 .8 1.8 1.8 1.8h33.5c2 0 3.9 1.1 4.8 3.4H19.2V41.8zm89.6 53.5H66c.8-2.2 2.8-3.4 4.8-3.4h33.5c1 0 1.8-.8 1.8-1.8V41.8h2.8l-.1 53.5z"
+					/><path
+						d="M32.7 50h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H32.7c-1 0-1.8.8-1.8 1.8s.8 1.8 1.8 1.8zM32.7 59.3h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H32.7c-1 0-1.8.8-1.8 1.8s.8 1.8 1.8 1.8zM32.7 68.7h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H32.7c-1 0-1.8.8-1.8 1.8s.8 1.8 1.8 1.8zM32.7 78h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H32.7c-1 0-1.8.8-1.8 1.8s.8 1.8 1.8 1.8zM72.8 50h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H72.8c-1 0-1.8.8-1.8 1.8s.9 1.8 1.8 1.8zM72.8 59.3h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H72.8c-1 0-1.8.8-1.8 1.8s.9 1.8 1.8 1.8zM72.8 68.7h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H72.8c-1 0-1.8.8-1.8 1.8s.9 1.8 1.8 1.8zM72.8 78h22.5c1 0 1.8-.8 1.8-1.8s-.8-1.8-1.8-1.8H72.8c-1 0-1.8.8-1.8 1.8s.9 1.8 1.8 1.8z"
+					/></svg
 				>
-					<path d="M8 2h40a4 4 0 0 1 4 4v52a4 4 0 0 1-4 4H8a4 4 0 0 1-4-4V6a4 4 0 0 1 4-4z" />
-					<path d="M8 2v60M48 2v60M8 18h40" />
-					<path d="M24 18v44M32 18v44" />
-				</svg>
 				<h3 class="secondaryHeading" style="margin-top: 20px;">Tutorials</h3>
 				<p>
 					Learning materials from industry experts. From building your first game to training your
@@ -332,12 +354,12 @@
 		</div>
 
 		<div class="section">
-			<h3 class="secondaryHeading">Describe Your Task, We’ll Handle the Rest.</h3>
+			<h3 class="secondaryHeading">Describe Your Task, We’ll Handle the Rest</h3>
 			<p>Create anything: graphic design, architecture, games, and more.</p>
 		</div>
 
 		<div class="section">
-			<h3 class="secondaryHeading">The perfect UI to create with AI.</h3>
+			<h3 class="secondaryHeading">The perfect UI to create with AI</h3>
 			<p>Stay in your creative flow with no complex setups or controls.</p>
 			<button class="primaryButton">Get started - it's free</button>
 		</div>
@@ -478,5 +500,11 @@
 	.grid .section {
 		background-color: hsl(0, 0%, 95%);
 		border-radius: 10px;
+	}
+
+	@media screen and (max-width: 700px){
+		.grid {
+		grid-template-columns: 1fr;
+	}
 	}
 </style>
