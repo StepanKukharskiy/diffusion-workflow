37ced35d96c495977accd075e93785bee2073734
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2025-02-01 16:11:46 +0000

    mobile blog version

--- src/lib/MainPageNavPanel.svelte
+++ src/lib/MainPageNavPanel.svelte
@@ -1,77 +1,121 @@
 <script lang='ts'>
     import kodiia_small from '$lib/logos/kodiia_small.svg';
+    import { slide } from 'svelte/transition';
+
+    let isMenuOpen = $state(false);
+
+    function toggleMenu() {
+        isMenuOpen = !isMenuOpen;
+    }
 </script>
+
 <div class="nav-container">
     <nav>
-        <div style="width: 100%; display: flex; align-items: center; justify-content: space-between;">
+        <div class="nav-content">
             <a href="https://kodiia.com">
                 <img src={kodiia_small} style="border-radius: 0" height="30" alt="logo" />
             </a>
-            <div style="display: flex; align-items: center;">
-                <button
-                    class="tertiaryButton"
-                    onclick={() => {
-                        window.open('/');
-                    }}>About</button
-                >
-                <button
-                    class="tertiaryButton"
-                    onclick={() => {
-                        window.open('/');
-                    }}>Docs</button
-                >
-                <button
-                    class="tertiaryButton"
-                    onclick={() => {
-                        window.open('/');
-                    }}>Pricing</button
-                >
-                <button
-                    class="tertiaryButton"
-                    onclick={() => {
-                        window.open('/blog', "_self");
-                    }}>Blog</button
-                >
-                <button
-                    class="primaryButton"
-                    style="margin-left: 10px;"
-                    onclick={() => {
-                        window.open('/threads', '_self');
-                    }}>Get started</button
-                >
+            <div class="desktop-menu">
+                <button class="tertiaryButton" onclick={() => { window.open('/'); }}>About</button>
+                <button class="tertiaryButton" onclick={() => { window.open('/'); }}>Docs</button>
+                <button class="tertiaryButton" onclick={() => { window.open('/'); }}>Pricing</button>
+                <button class="tertiaryButton" onclick={() => { window.open('/blog', "_self"); }}>Blog</button>
+                <button class="primaryButton" style="margin-left: 10px;" onclick={() => { window.open('/threads', '_self'); }}>Get started</button>
             </div>
+            <button class="mobile-menu-icon" onclick={toggleMenu} aria-label='menu button'>
+                &#9776; <!-- Hamburger icon -->
+            </button>
         </div>
+        
     </nav>
+    {#if isMenuOpen}
+            <div class="mobile-menu" transition:slide>
+                <button class="tertiaryButton" onclick={() => { window.open('/'); }}>About</button>
+                <button class="tertiaryButton" onclick={() => { window.open('/'); }}>Docs</button>
+                <button class="tertiaryButton" onclick={() => { window.open('/'); }}>Pricing</button>
+                <button class="tertiaryButton" onclick={() => { window.open('/blog', "_self"); toggleMenu()}}>Blog</button>
+                <button class="primaryButton" onclick={() => { window.open('/threads', '_self'); toggleMenu() }}>Get started</button>
+            </div>
+        {/if}
 </div>
 
 <style>
     .nav-container {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		z-index: 99;
-	}
-	nav {
-		min-height: 40px;
-		width: calc(100% - 20px);
-		max-width: 1200px;
-		box-sizing: border-box;
-		border-radius: 10px;
-		background: #fdfdfd;
-		background: linear-gradient(45deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.05));
-		backdrop-filter: blur(25px);
-		-webkit-backdrop-filter: blur(25px);
-		padding: 10px;
-		margin: 10px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 99;
+    }
+    nav {
+        min-height: 40px;
+        width: calc(100% - 20px);
+        max-width: 1200px;
+        box-sizing: border-box;
+        border-radius: 10px;
+        background: #fdfdfd;
+        background: linear-gradient(45deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.05));
+        backdrop-filter: blur(25px);
+        -webkit-backdrop-filter: blur(25px);
+        padding: 10px;
+        margin: 10px;
+        box-shadow: 0 0 10px hsl(0, 0%, 70%);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        z-index: 99;
+    }
+    .nav-content {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .desktop-menu {
+        display: flex;
+        align-items: center;
+    }
+    .mobile-menu-icon {
+        display: none;
+        cursor: pointer;
+        font-size: 24px;
+        background: none;
+        border: none;
+    }
+    .mobile-menu {
+        display: none;
+        position: fixed;
+        top: 75px;
+        left: 0;
+        margin: 0 10px;
+        width: calc(100% - 20px);
+        height: calc(100vh - 85px);
+        background: linear-gradient(45deg, rgba(255, 255, 255, 0.52), rgba(255, 255, 255, 0.25));
+		backdrop-filter: blur(40px);
+		-webkit-backdrop-filter: blur(40px);
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
 		box-shadow: 0 0 10px hsl(0, 0%, 70%);
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		z-index: 99;
-	}
+		z-index: 10;
+    }
+    .mobile-menu button {
+        margin: 10px 0;
+    }
+    @media (max-width: 768px) {
+        .desktop-menu {
+            display: none;
+        }
+        .mobile-menu-icon {
+            display: block;
+        }
+        .mobile-menu {
+            display: flex;
+        }
+    }
 </style>
--- src/routes/blog/iterative-image-generation/+page.svelte
+++ src/routes/blog/iterative-image-generation/+page.svelte
@@ -18,57 +18,68 @@
 			LORAs. It’s a simple, flexible process that uses basic image generation and iterative
 			refinement to develop unique architectural concepts.
 		</p>
-		
+
 		<h4 style="margin-top: 20px;">Step 1: Generate a Basic Image</h4>
 		<p>
-			<span class='black-dot'></span> Start with an inspiration: a natural formation (e.g., a rock) or an everyday object (e.g., a
-			Game Boy).
-			<br /><span class='black-dot'></span> Generate a series of images based on this inspiration.
-			<br /><span class='black-dot'></span> Select the most interesting image as the foundation for further exploration.
+			<span class="black-dot"></span> Start with an inspiration: a natural formation (e.g., a rock)
+			or an everyday object (e.g., a Game Boy).
+			<br /><span class="black-dot"></span> Generate a series of images based on this inspiration.
+			<br /><span class="black-dot"></span> Select the most interesting image as the foundation for further
+			exploration.
 		</p>
 		<img src={img01} alt="rocks" />
-		<p>Model: Flux Schnell 
+		<p>
+			Model: Flux Schnell
 			<br />Prompt: A close up view of rocks formation, high res, no blur
 		</p>
-		
+
 		<h4 style="margin-top: 20px;">Step 2: Refine with ControlNet</h4>
 		<p>
-			<span class='black-dot'></span> Use the selected image as a reference for composition in ControlNet.
-			<br /><span class='black-dot'></span> Add prompts tailored to visualize architectural elements (e.g., buildings, pavilions).
-			<br /><span class='black-dot'></span> Generate another series of images based on these prompts.
+			<span class="black-dot"></span> Use the selected image as a reference for composition in
+			ControlNet.
+			<br /><span class="black-dot"></span> Add prompts tailored to visualize architectural elements
+			(e.g., buildings, pavilions).
+			<br /><span class="black-dot"></span> Generate another series of images based on these prompts.
 		</p>
 		<img src={img02} alt="rocks" />
-		<p>Model: Flux Canny Pro 
-			<br />Prompt: An image of a library building made of concrete shards, street view, clear sky, photo
+		<p>
+			Model: Flux Canny Pro
+			<br />Prompt: An image of a library building made of concrete shards, street view, clear sky,
+			photo
 		</p>
-		
+
 		<h4 style="margin-top: 20px;">Step 3: Iterate</h4>
 		<p>
-			<span class='black-dot'></span> Choose the most compelling image from the new series.
-			<br /><span class='black-dot'></span> Use it as a reference for the next iteration, adjusting prompts as needed.
+			<span class="black-dot"></span> Choose the most compelling image from the new series.
+			<br /><span class="black-dot"></span> Use it as a reference for the next iteration, adjusting prompts
+			as needed.
 		</p>
 		<img src={img03} alt="rocks" />
-		<p>Model: Flux Canny Pro 
-			<br />Prompt: An image of a library building made of concrete and metal, warm light inside, busy street view, clear sky, photo
+		<p>
+			Model: Flux Canny Pro
+			<br />Prompt: An image of a library building made of concrete and metal, warm light inside,
+			busy street view, clear sky, photo
 		</p>
 		<h4 style="margin-top: 20px;">Step 4: Evolve</h4>
 		<p>
-			<br /><span class='black-dot'></span> Repeat this process for 3–4 iterations to refine the design further, exploring new
-			ideas or enhancing specific features each time.
+			<br /><span class="black-dot"></span> Repeat this process for 3–4 iterations to refine the design
+			further, exploring new ideas or enhancing specific features each time.
 		</p>
 		<img src={img04} alt="rocks" />
-		<p>Model: Flux Canny Pro 
-			<br />Prompt: An image of a library building made of concrete and giant rocks, complex geometry, warm light inside, busy street view, clear sky, photo
+		<p>
+			Model: Flux Canny Pro
+			<br />Prompt: An image of a library building made of concrete and giant rocks, complex
+			geometry, warm light inside, busy street view, clear sky, photo
 		</p>
 		<h4 style="margin-top: 20px;">Why This Method Works</h4>
 		<p>
-			<span class='black-dot'></span>No LORAs or Fine-Tuned Models:
+			<span class="black-dot"></span>No LORAs or Fine-Tuned Models:
 			<br />The process relies entirely on standard tools and creative prompts, making it accessible
 			and flexible.
-			<br /><span class='black-dot'></span>Infinite Possibilities:
+			<br /><span class="black-dot"></span>Infinite Possibilities:
 			<br />While 3–4 iterations are usually enough, you can repeat this process indefinitely for
 			deeper exploration.
-			<br /><span class='black-dot'></span>Adaptable and Creative:
+			<br /><span class="black-dot"></span>Adaptable and Creative:
 			<br />Each step builds on the last, often leading to surprising and innovative results.
 		</p>
 		<p>
@@ -80,7 +91,6 @@
 	</div>
 	<Footer />
 </div>
-
 
 <style>
 	.blog-container {
@@ -104,6 +114,7 @@
 		text-wrap: wrap;
 	}
 	.blog-data img {
+		width: 100%;
 		max-width: 400px;
 	}
 </style>
