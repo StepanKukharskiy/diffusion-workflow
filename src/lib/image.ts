c358f43672d57c34c2bcc71931172d7cb2d2bee9
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2025-03-07 07:35:50 +0000

    model update

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -56,7 +56,7 @@ export async function imageResponse(model = '', query = '', referenceComposition
                         safety_tolerance: 2,
                         prompt_upsampling: false
                     }
-                    const [output] = await replicate.run("stepankukharskiy/flux-crmcs:24ef6eabcf2cd5b871a4776efccb15d80379902e4ac9d0c1c5ca8532ac966c1d", { input });
+                    const [output] = await replicate.run("stepankukharskiy/flux-crmcs:4c34c922877647dd13753e65b2774fc78c817c33a700eacb817dedd60d696fad", { input });
                     const imageUrl = output.url().href;
                     response = {
                         imageUrl: imageUrl
